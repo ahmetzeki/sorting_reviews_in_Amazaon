76f4eb54c2dff835c7f4880a4c2b6887f6917e32
Ahmet Zeki Albayrak <ahmet.zeki.albayrak@gmail.com>
2023-07-14 02:59:48 +0300

    Readme file was created

---
+++ README.md
@@ -0,0 +1,40 @@
+<h2 align="center">Sorting Reviews in Amazon</h3>
+<br>
+<br>
+<img algin="center" src="https://github.com/TarikKaanKoc/Rating.Product-and-Sorting.Reviews-in-Amazon/blob/main/image.jpeg"/>
+<h3 align="center">Business Problem</h3>
+
+
+
+One of the most important problems in e-commerce is the correct calculation of the points given to the product after 
+sales. The solution to this problem means to provide customer satisfaction for e-commerce site, to make product to 
+stand out for sellers and hassle-free shopping experience for buyers.
+
+Another problem is correct ordering of the comments given to the product. Occurrence of misleading
+comments can have direct impact on product sales, leading to the loss of both customers
+and financial resources. As the e-commerce site and sellers tackle these two core challenges, 
+they can boost their sales while ensuring a smooth and hassle-free purchasing journey for customers
+
+
+### Story of Dataset
+
+This dataset contains categories and various metadatas of product in Amazon. It has the user 
+ratings and reviews of the electronic category's most reviewed product
+
+
+### Variables:
+
+Index | Column          | Description |
+--- |-----------------| --- | 
+1 | reviewerID      | User Id| 
+2 | asin            | Product Id | 
+3 | reviewerName    | User Name | 
+4 | helpful         | Useful Evaluation Degree | 
+5 | reviewText      | Evaluation |
+6 | overall         | Product Rating |
+7 | summary         | Evaluation Summary |
+8 | unixReviewTime  | Evaluation Time |
+9 | reviewTime      | Evaluation Time {RAW} |
+10 | days - day_diff | Number of days since assessment |
+11 | helpful_yes     | The number of times the evaluation was found useful |
+12 | total_vote      | Number of votes given to the evaluation |
---
+++ Rating Product & Sorting Reviews in Amazon.py
@@ -0,0 +1,80 @@
+
+###################################################
+# PROJE: Rating Product & Sorting Reviews in Amazon
+###################################################
+
+###################################################
+# Business problem
+###################################################
+
+
+###################################################
+# Veri Seti Hikayesi
+###################################################
+
+# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
+# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
+
+# Değişkenler:
+# reviewerID: Kullanıcı ID’si
+# asin: Ürün ID’si
+# reviewerName: Kullanıcı Adı
+# helpful: Faydalı değerlendirme derecesi
+# reviewText: Değerlendirme
+# overall: Ürün rating’i
+# summary: Değerlendirme özeti
+# unixReviewTime: Değerlendirme zamanı
+# reviewTime: Değerlendirme zamanı Raw
+# day_diff: Değerlendirmeden itibaren geçen gün sayısı
+# helpful_yes: Değerlendirmenin faydalı bulunma sayısı
+# total_vote: Değerlendirmeye verilen oy sayısı
+
+
+
+###################################################
+# GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
+###################################################
+
+# Paylaşılan veri setinde kullanıcılar bir ürüne puanlar vermiş ve yorumlar yapmıştır.
+# Bu görevde amacımız verilen puanları tarihe göre ağırlıklandırarak değerlendirmek.
+# İlk ortalama puan ile elde edilecek tarihe göre ağırlıklı puanın karşılaştırılması gerekmektedir.
+
+
+###########################################
+# ########
+# Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
+###################################################
+
+
+###################################################
+# Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
+###################################################
+
+
+
+
+###################################################
+# Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
+###################################################
+
+
+###################################################
+# Adım 1. helpful_no Değişkenini Üretiniz
+###################################################
+
+# Not:
+# total_vote bir yoruma verilen toplam up-down sayısıdır.
+# up, helpful demektir.
+# veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
+
+
+###################################################
+# Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
+###################################################
+
+
+##################################################
+# Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
+###################################################
+
+
